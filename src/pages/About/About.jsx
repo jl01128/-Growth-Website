3fd89c5b57ac9dbff2c4c4e2685e3d4dcabf8f92
jl01128 <jackie01128@gmail.com>
2024-02-12 13:25:08 -0500

    further updates to file and logo

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             <Navbar></Navbar>
             <div className="about-body" >
                 <h2>About</h2>
-                <p>Growth is a mobile application designed for primary school students around the world. This app provides supplemental math education focusing on fundamental math concepts including addition, subtraction, multiplication, and division. 
+                <p>Explore is a mobile application designed for primary school students around the world. This app provides supplemental math education focusing on fundamental math concepts including addition, subtraction, multiplication, and division. 
                 The structure of our content follows the curriculum of the public school system in Chad and is based on the first four years of math education. 
                 Designed specifically for areas with little to no internet, our application works fully 
                 offline and uses limited storage on smartphones. We also have the ability to add more local languages.</p>
