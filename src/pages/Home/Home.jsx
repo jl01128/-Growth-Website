353df9f546a97457c5b19315deb453981b577e3f
jl01128 <jackie01128@gmail.com>
2024-02-15 20:33:22 -0500

    additional updates and organization of files

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import Navbar from "../../components/Navbar";
 
 const Home = () => {
     return (
-        <div className="home" style={{ backgroundImage: 'url("/homeBackground.png")', backgroundSize: 'cover', backgroundPosition: 'center', height: '100%' }}>
+        <div className="home" style={{ backgroundImage: 'url("/homeBackground.png")', alignContent: "center"}}>
             <Helmet bodyAttributes={{style: 'background-color: #00093B'}}/>
             <Navbar></Navbar>
             <div className="home-body">
